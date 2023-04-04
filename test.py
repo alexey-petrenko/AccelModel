e8126975bccc9198d12c289c856865e79de3fd1c
Alexey Petrenko <alexey.petrenko@gmail.com>
2023-04-04 22:02:31 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import elements as am
 
 Q1 = am.Quadrupole(L=0.2, K1=2.0, name="Q1")
 
-print(f"{Q1.type_name}: name={Q1.name}, K1={Q1.K1} m, location={Q1.s0} m")
+print(f"{Q1.type_name}: name={Q1.name}, K1={Q1.K1} 1/m^2, location={Q1.s0} m")
