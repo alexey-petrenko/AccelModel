f0a84cc782a1000ac534aa72797556bafbca5d34
Alexey Petrenko <alexey.petrenko@gmail.com>
2023-04-05 16:56:03 +0700

    UniformElement changed to  DivisibleElement

--- elements.py
+++ elements.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class AccElement:
-    def __init__(self, L=0, s0=0, name=None):
-        self.L  = L  # m
-        self.s0 = s0  # m -- location of the element center along the beamline
+    def __init__(self, L=0, s=0, name=None):
+        self.L  = L # m
+        self.s = s  # m -- location of the element center along the beamline
         self.name = name
         self.type_name = None
         
@@ -18,35 +18,36 @@ class AccElement:
             [0, 0, 0, 0, 0, 1]
         ])
 
-class UniformElement(AccElement):
-    def M(self, L=None):
-        if L is None: L = self.L
+class DivisibleElement(AccElement):
+    def M(self, l=None):
+        # l is the distance to the element start (inside the element)
+        if l is None: l = self.L
 
         return np.matrix([
-            [1, L, 0, 0, 0, 0],
+            [1, l, 0, 0, 0, 0],
             [0, 1, 0, 0, 0, 0],
-            [0, 0, 1, L, 0, 0],
+            [0, 0, 1, l, 0, 0],
             [0, 0, 0, 1, 0, 0],
             [0, 0, 0, 0, 1, 0],
             [0, 0, 0, 0, 0, 1]
         ])
       
-class Quadrupole(UniformElement):
+class Quadrupole(DivisibleElement):
     def __init__(self, *args, K1=0, **kwargs):
         super().__init__(*args, **kwargs)
         self.K1 = K1 # 1/m^2 -- geometric strength of quadrupole
         self.type_name = "Quadrupole"
 
-    def M(self, L=None):
-        if L is None: L = self.L
+    def M(self, l=None):
+        if l is None: l = self.L
         
         K1 = self.K1
         
         if K1 == 0:
             return np.matrix([
-                [1, L, 0, 0, 0, 0],
+                [1, l, 0, 0, 0, 0],
                 [0, 1, 0, 0, 0, 0],
-                [0, 0, 1, L, 0, 0],
+                [0, 0, 1, l, 0, 0],
                 [0, 0, 0, 1, 0, 0],
                 [0, 0, 0, 0, 1, 0],
                 [0, 0, 0, 0, 0, 1]
@@ -54,58 +55,58 @@ class Quadrupole(UniformElement):
         
         k = np.sqrt(np.abs(K1))
         
-        sinkL  = np.sin(k*L)
-        coskL  = np.cos(k*L)
-        sinhkL = np.sinh(k*L)
-        coshkL = np.cosh(k*L)
+        sinkl  = np.sin(k*l)
+        coskl  = np.cos(k*l)
+        sinhkl = np.sinh(k*l)
+        coshkl = np.cosh(k*l)
 
         if K1 > 0:
             return np.matrix([
-                [   coskL, sinkL/k,  0,      0,     0, 0],
-                [-k*sinkL, coskL,    0,      0,     0, 0],
-                [    0,     0,    coshkL, sinhkL/k, 0, 0],
-                [    0,     0,  k*sinhkL, coshkL,   0, 0],
+                [   coskl, sinkl/k,  0,      0,     0, 0],
+                [-k*sinkl, coskl,    0,      0,     0, 0],
+                [    0,     0,    coshkl, sinhkl/k, 0, 0],
+                [    0,     0,  k*sinhkl, coshkl,   0, 0],
                 [    0,     0,       0,      0,     1, 0],
                 [    0,     0,       0,      0,     0, 1]
             ])
         
         if K1 < 0:
             return np.matrix([
-                [  coshkL, sinhkL/k,  0,     0,      0, 0],
-                [k*sinhkL, coshkL,    0,     0,      0, 0],
-                [    0,     0,      coskL, sinkL/k,  0, 0],
-                [    0,     0,   -k*sinkL, coskL,    0, 0],
+                [  coshkl, sinhkl/k,  0,     0,      0, 0],
+                [k*sinhkl, coshkl,    0,     0,      0, 0],
+                [    0,     0,      coskl, sinkl/k,  0, 0],
+                [    0,     0,   -k*sinkl, coskl,    0, 0],
                 [    0,     0,       0,      0,      1, 0],
                 [    0,     0,       0,      0,      0, 1]
             ])
         
         return None
 
-class Solenoid(UniformElement):
+class Solenoid(DivisibleElement):
     def __init__(self, K=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.K = K # 1/m
         self.type_name = "Solenoid"
 
-    def M(self, L=None):
+    def M(self, l=None):
 
-        if L is None: L = self.L
+        if l is None: l = self.L
 
         K = self.K
 
         if K == 0:
             return np.matrix([
-                [1, L, 0, 0, 0, 0],
+                [1, l, 0, 0, 0, 0],
                 [0, 1, 0, 0, 0, 0],
-                [0, 0, 1, L, 0, 0],
+                [0, 0, 1, l, 0, 0],
                 [0, 0, 0, 1, 0, 0],
                 [0, 0, 0, 0, 1, 0],
                 [0, 0, 0, 0, 0, 1]
             ])
 
 
-        S = np.sin(K*L)
-        C = np.cos(K*L)
+        S = np.sin(K*l)
+        C = np.cos(K*l)
 
         return np.matrix([
             [ C**2,   (S*C)/K,  S*C,   (S**2)/K, 0, 0],
