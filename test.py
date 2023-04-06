006acb855a3e00283ea817e4f2864ead1fb3d16e
Daniil <reshetov.daniil@inbox.ru>
2023-04-06 12:48:08 +0700

    update test.pt

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ Sol1 = am.Solenoid(L=0.5, K=1.4, name="Sol1", s=2.0)
 print(f"{Sol1.type_name}: name={Sol1.name}, K={Sol1.K} 1/m, location={Sol1.s} m")
 print(f"Sol1 matrix = \n{Sol1.M()}\n")
 
-S_bend1 = am.Sector_bend(L=0.6, alpha=0.31, name="S_bend1", s=2)
+S_bend1 = am.SectorBend(L=0.6, alpha=0.31, name="S_bend1", s=2)
 print(f"{S_bend1.type_name}: name={S_bend1.name}, alpha={S_bend1.alpha} rad, location={S_bend1.s} m")
 print(f"S_bend1 matrix = \n{S_bend1.M()}\n")
 
